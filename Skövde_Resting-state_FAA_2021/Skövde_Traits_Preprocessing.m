1163473723d23ac53522f47406679ca15ab7886e
Martin Nilsson <68947474+Zamsobob@users.noreply.github.com>
2021-02-14 15:36:07 +0100

    Update Skövde_Traits_Preprocessing.m

--- Skövde_Resting-state_FAA_2021/Skövde_Traits_Preprocessing.m
+++ Skövde_Resting-state_FAA_2021/Skövde_Traits_Preprocessing.m
@@ -16,10 +16,20 @@ numsubjects = length(subject_list);
 % Path to the parent folder, which contains the data folders for all subjects
 parentfolder = 'D:\FAA_Study_2021\Skovde\Skovde_Traits_FAA\Skovde_EEG\EEG_RAW\';
 
+% Path to EEGlab template channel location
+chanlocs = 'C:\Users\Mar Nil\Desktop\MATLABdirectory\eeglab2020_0\plugins\dipfit3.7\standard_BESA\standard-10-5-cap385.elp';
+
+% Localizer file
+localizer = 'D:\FAA_Study_2021\Skovde\Skovde_Traits_FAA\Skovde_EEG\EEG_Localizer\';
+
 % Initialize the ALLERP structure and CURRENTERP
 % ALLERP = buildERPstruct([]);
 % CURRENTERP = 0;
 % ALLEEG? [ALLEEG EEG CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+
+% Create folder
+mkdir 'D:\FAA_Study_2021\Skovde\Skovde_Traits_FAA\Skovde_EEG\' EEG_test;
+newdir = 'D:\FAA_Study_2021\Skovde\Skovde_Traits_FAA\Skovde_EEG\EEG_test';
 
 % Loop through all subjects
 for s = 1:numsubjects
@@ -29,26 +39,41 @@ for s = 1:numsubjects
     % Path to the folder containing the current subject's data
     subjectfolder = [ parentfolder subject '\'];
 
-    % Import data. FIX.
-    EEG = pop_importdata('dataformat','matlab','nbchan',0,'data',[subjectfolder subject '.mat'],'srate',512,'pnts',0,'xmin',0);
+    % Import data.
+    EEG = pop_importdata('dataformat','matlab','nbchan',35,'data',[subjectfolder subject '.mat'],'srate',512,'pnts',0,'xmin',0);
     
     % Import event information (channel 18)
     % Where is the resting-state "trial" ? D? Talk to B. See Turku_Step1
-    % Event 4 is resting-state? EEG.event
-    % EEG = pop_chanevent(EEG, 18, 'edge', 'leading', 'edgelen', 0 );
+    % Event 4 is resting-state??
+    EEG = pop_chanevent(EEG, 18, 'edge', 'leading', 'edgelen', 0 );
+    
     
     % Remove channel 1 (g.tec time channel) and 35 (empty channel).
-    EEG = pop_select( EEG, 'nochannel', [1 35] );
+    % Find in GUI.
+    EEG = pop_select( EEG, 'nochannel', [1 34] );
     
-    % Trim dataset?
+    % Import channel locations. ERROR
+    EEG = pop_chanedit(EEG, 'lookup', chanlocs,'load',{[ localizer 'Locations_32Channels.ced'] 'filetype' 'autodetect'});
+    
+    % Re-reference to LM RM
+    EEG = pop_reref( EEG, [5 6] );
+
+    % Trim dataset
     % EEG  = pop_eegtrim( EEG, 0, 3000 , 'post',  3000, 'pre',  0 );
     
     % Resample dataset from 512 to 256 Hz.
     EEG = pop_resample(EEG, 256);
     
-    % Channel locations, filter etc.
     
-    % Make new folders (preprocess, .set, etc). mkdir:
-    % https://www.mathworks.com/help/matlab/ref/mkdir.html
+    % High-pass filter the data at 1 Hz. Note that EEGLAB uses pass-band edge, therefore 1/2 = 0.5 Hz.
+    EEG = pop_eegfiltnew(EEG, 1, [], 1650, 0, [], 0);
+    
+    % Low-pass filter the data at 40 Hz.
+    EEG = pop_eegfiltnew(EEG, [], 40, 166, 0, [], 0);
+
+    % Save in created folder
+    EEG = pop_editset(EEG, 'setname', [subject '_Preprocess']);
+    EEG = pop_saveset( EEG, 'filename', [subject '_Preprocess.set'],'filepath', newdir);
+    
 end
      
