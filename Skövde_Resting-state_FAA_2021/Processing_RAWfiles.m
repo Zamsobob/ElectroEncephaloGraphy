592a48d670a228eaa864132c876a67a10947f414
Martin Nilsson <68947474+Zamsobob@users.noreply.github.com>
2021-02-17 21:17:43 +0100

    Update Processing_RAWfiles.m
    A lot to figure out, but the next step is ICA.

--- Skövde_Resting-state_FAA_2021/Processing_RAWfiles.m
+++ Skövde_Resting-state_FAA_2021/Processing_RAWfiles.m
@@ -19,7 +19,7 @@ eegfolder = 'D:\FAA_Study_2021\Skovde\Skovde_EEG\';
 rawfolder = 'D:\FAA_Study_2021\Skovde\Skovde_EEG\EEG_RAW\';
 
 % PATH TO EEGLAB TEMPLATE (BESA) CAP
-chanlocs = 'C:\Users\Mar Nil\Desktop\MATLABdirectory\eeglab2020_0\plugins\dipfit3.7\standard_BESA\standard-10-5-cap385.elp';
+chanlocation = 'C:\Users\Mar Nil\Desktop\MATLABdirectory\eeglab2020_0\plugins\dipfit3.7\standard_BESA\standard-10-5-cap385.elp';
 
 %PATH TO LOCALIZER FILE (INCLUDES CHANNEL LOCATIONS)
 localizer = 'D:\FAA_Study_2021\Skovde\Skovde_EEG\EEG_Localizer\';
@@ -48,7 +48,7 @@ ecdir = [ eegfolder 'EEG_RS\RS_EC'];
 %% PREPROCESSING OF RAW DATA
 
 % LOOP THROUGH ALL SUBJECTS
-for s = 1 %:numsubjects
+for s = 1:numsubjects
     
     subject = subject_list{s};
     
@@ -64,8 +64,8 @@ for s = 1 %:numsubjects
     % REMOVE FIRST(G.TEC TIME) AND LAST (EMPTY) CHANNELS
     EEG = pop_select( EEG, 'nochannel', [1 34] );
     
-    % IMPORT CHANNEL LOCATIONS
-    EEG = pop_chanedit(EEG, 'lookup', chanlocs,'load',{[ localizer 'Locations_32Channels.ced'] 'filetype' 'autodetect'});
+    % IMPORT CHANNEL LOCATIONS AND OPTIMIZE HEAD CENTER
+    EEG = pop_chanedit(EEG, 'lookup', chanlocation,'load',{[ localizer 'Locations_32Channels.ced'] 'filetype' 'autodetect'},'eval','chans = pop_chancenter( chans, [],[]);');
     
     % RE-REFERENCE TO LM RM (FOR NOW)
     EEG = pop_reref( EEG, [5 6] );
@@ -76,14 +76,12 @@ for s = 1 %:numsubjects
     % RESAMPLE DATASET FROM 512 TO 256 HZ
     EEG = pop_resample(EEG, 256);
     
-    % HIGH-PASS FILTER THE DATA AT 1 HZ. --Clean rawdata and ASR?--
-    EEG = pop_eegfiltnew(EEG, 'locutoff',1);
-    
-    % LOW-PASS FILTER THE DATA AT 40 HZ
-    EEG = pop_eegfiltnew(EEG, 'hicutoff',40);
-    
-    % CLEAN DATA
-    % EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
+    % NOTCH FILTER 50 HZ TO REMOVE LINE NOISE
+    EEG  = pop_basicfilter( EEG,  1:EEG.nbchan, ... 
+        'Filter', 'PMnotch', ...
+        'Design', 'notch', ...
+        'Cutoff', 50, ...
+        'Order', 180 );
     EEG.setname = subject;
     
     %% EXTRACT RESTING-STATE AND STATE-DEPENDENT DATA
@@ -114,14 +112,30 @@ for s = 1 %:numsubjects
     EEG_RSFAA = pop_saveset( EEG_RSFAA, 'filename',[subject '_RS.set'],'filepath', rsdir);
     EEG_SDFAA = pop_saveset( EEG_SDFAA, 'filename',[subject '_SD.set'],'filepath', sddir);
     
-    % GOOD PLACE TO EXAMINE THE DATA
+    %% PREPROCESSING RESTING-STATE DATA
+    
+    % OPEN RS FILE FROM PREVIOUS STEP
+    EEG = pop_loadset( 'filename',[ subject '_RS.set'],'filepath', rsdir);
+    
+    % HIGH-PASS FILTER THE DATA AT 1 HZ.
+    EEG = pop_eegfiltnew(EEG, 'locutoff',1);
+    EEG.setname = [ subject '_RS_Filt'];
+    
+    % SAVE FILTERED RS DATA FOR VISUAL EXAMINATION
+    EEG = pop_saveset( EEG, 'filename',[subject '_RS_Filt.set'],'filepath', rsdir);
+    
+    % SAVE CHANNELS BEFORE REMOVING BAD ONES
+    originalchanlocs = EEG.chanlocs;
+    
+    % USE CLEAN_RAW TO REMOVE BAD CHANNELS
+    EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion','off','WindowCriterion','off','BurstRejection','off','Distance','Euclidian');
     
 %% EPOCHING
     
 %%%%% EYES OPEN %%%%%%
     
     % OPEN RS FILE FROM PREVIOUS STEP
-    EEG = pop_loadset( 'filename',[ subject '_RS.set'],'filepath', rsdir);
+    % EEG = pop_loadset( 'filename',[ subject '_RS.set'],'filepath', rsdir);
      
     % CREATE 1 MINUTE EPOCHS OF EYES OPEN (EO) CONDITION. EVENT CODE 30
     EEG_EO = pop_epoch( EEG, {  '30'  }, [0         59.9], 'newname', [ subject '_EO'], 'epochinfo', 'yes');
@@ -147,7 +161,7 @@ for s = 1 %:numsubjects
     % CONCATENATE THE EO EPOCHS
     EEG_EC = pop_epoch2continuous(EEG_EC, 'Warning', 'off');
     
-    % CREATE CONTINOUS EO EPOCHS OG 2.048 DRVONFD, WITH 75% OVERLAP (0.512)
+    % CREATE CONTINOUS EO EPOCHS OG 2.048 SEC, WITH 75% OVERLAP (0.512)
     EEG_EC = eeg_regepochs(EEG_EC, 'recurrence', 0.512, 'limits', [-1.024 1.024], 'rmbase', NaN); 
      
     % REMOVE BASELINE (MEAN OF THE WHOLE EPOCH)
@@ -176,52 +190,35 @@ for s = 1 %:numsubjects
     
     %% RUN ICA ON EEG CHANNELS
     
-    EEG_EO = pop_runica(EEG_EO, 'extended',1,'interupt','on','pca', length(EEG_EO.chanlocs));
-    EEG_EC = pop_runica(EEG_EC, 'extended',1,'interupt','on','pca', length(EEG_EC.chanlocs));
-    EEG_EO.setname = [ subject '_EO_ICA']
-    EEG_EC.setname = [ subject '_EC_ICA']
-    
-    % SAVE DATA WITH ICA WEIGHTS
-    EEG_EO = pop_saveset( EEG_EO, 'filename',[ subject '_EO_ICA.set'],'filepath', eodir);
-    EEG_EC = pop_saveset( EEG_EC, 'filename',[ subject '_EC_ICA.set'],'filepath', ecdir);
-    
-    %% MARA
-    
-    % INITIALIZE ALLEEG BY OPENING EEGLAB
-    [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-    
-    % ADD CURRENT EEG TO ALLEEG BY RELOADING SAVED ICA DATA
-    EEG_EO = pop_loadset( 'filename',[ subject '_EO_ICA.set'],'filepath', eodir);
-    EEG_EC = pop_loadset( 'filename',[ subject '_EC_ICA.set'],'filepath', ecdir);
-    EEG_EO = eeg_checkset(EEG_EO);
-    EEG_EC = eeg_checkset(EEG_EC);
-    
-    % AUTOMATICALLY REMOVE COMPONENTS WITH MARA
-    EEG_EO = processMARA (ALLEEG, EEG_EO, CURRENTSET, [0,0,0,0,1]);
-    EEG_CO = processMARA (ALLEEG, EEG_EC, CURRENTSET, [0,0,0,0,1]);
-    
-    % automatically remove artifacts without gui
-    % EEG = pop_subcomp(EEG);
+%     EEG_EO = pop_runica(EEG_EO, 'extended',1,'interupt','on','pca', length(EEG_EO.chanlocs));
+%     EEG_EC = pop_runica(EEG_EC, 'extended',1,'interupt','on','pca', length(EEG_EC.chanlocs));
+%     EEG_EO.setname = [ subject '_EO_ICA']
+%     EEG_EC.setname = [ subject '_EC_ICA']
+%     
+%     % SAVE DATA WITH ICA WEIGHTS
+%     EEG_EO = pop_saveset( EEG_EO, 'filename',[ subject '_EO_ICA.set'],'filepath', eodir);
+%     EEG_EC = pop_saveset( EEG_EC, 'filename',[ subject '_EC_ICA.set'],'filepath', ecdir);
 
-    % SAVE DATA AFTER REMOVING COMPONENTS
-    EEG_EO = pop_saveset( EEG_EO, 'filename',[ subject '_EO_MARA.set'],'filepath', eodir);
-    EEG_EC = pop_saveset( EEG_EC, 'filename',[ subject '_EC_MARA.set'],'filepath', ecdir);
-    EEG_EO.setname = [ subject '_EO_MARA']
-    EEG_EC.setname = [ subject '_EC_MARA']
+    %% Interpolate, EOGs, IClabel. Small number of time points?
     
     % ----------------------------
-    % Interpolate channels with original channel locations (before ICA?)
+    % Interpolate channels with original channel locations
     % EEG_EO = pop_interp(EEG, EEG.originalEEG.chanlocs, 'spherical');
     % EEG_EC = pop_interp(EEG, EEG.originalEEG.chanlocs, 'spherical');
     
-    
-    % CLEAN RAWDATA? (AFTER/INSTEAD OF HIGH-PASS FILTER AND BEFORE ICA)
-    % EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion',5,'ChannelCriterion',0.8,'LineNoiseCriterion',4,'Highpass','off','BurstCriterion',20,'WindowCriterion',0.25,'BurstRejection','on','Distance','Euclidian','WindowCriterionTolerances',[-Inf 7] );
-
     % TO DO: EOG channels. Reject data/channels. Interpolate bad
     % electrodes. Clean rawdata. Reject epochs (manually?). Before ICA. 
     % Separate into RS and SD before anything else perhaps? Maybe that is
     % where one event is disappearing? EEG = eeg_interp? Fix MARA.
+    
+    % REMOVE EYE-CHANNELS (AFTER ICA)
+    % EEG = pop_select( EEG,'nochannel',{'EOG1' 'EOG2' 'EOG3' 'EOG4'});
+    
+    % USE CLEANLINENOISE FROM THE PREP PIPELINE
+    %     EEG = pop_clean_rawdata(EEG, 'FlatlineCriterion','off','ChannelCriterion','off','LineNoiseCriterion','off','Highpass',[0.25 1.25] ,'BurstCriterion','off','WindowCriterion','off','BurstRejection','off','Distance','Euclidian');
+
+    % Run ICLabel (Pion-Tonachini et al., 2019)
+    % EEG = iclabel(EEG, 'default');
     
 end
 
