ef1fab83795776004fd8678084c1313e0dd3827f
Martin Nilsson <68947474+Zamsobob@users.noreply.github.com>
2021-03-09 20:11:39 +0100

    Update RS_CSD_Transformation.m
    eeglab v2021

--- Skövde_Resting-state_FAA_2021/Reference_Free/RS_CSD_Transformation.m
+++ Skövde_Resting-state_FAA_2021/Reference_Free/RS_CSD_Transformation.m
@@ -3,7 +3,7 @@ clc;
 %% SET UP FILES AND FOLDERS
 
 % MAKE SURE EEGLAB IS IN PATH
-addpath('C:\Users\Mar Nil\Desktop\MATLABdirectory\eeglab2020_0');
+addpath('C:\Users\Mar Nil\Desktop\MATLABdirectory\eeglab2021.0');
 % WORKING DIRECTORY
 cd 'D:\FAA_Study_2021\Skovde\Skovde_EEG'
 
