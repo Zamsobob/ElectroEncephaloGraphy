a12be9477cf3f6ba849e5871930107f631cf5b78
Martin Nilsson <68947474+Zamsobob@users.noreply.github.com>
2021-04-13 12:11:11 +0100

    Update FAA_Statistics.R

--- LEMON_dataset_FAA/FAA_Statistics/FAA_Statistics.R
+++ LEMON_dataset_FAA/FAA_Statistics/FAA_Statistics.R
@@ -1,71 +1,49 @@
-# BEST PRACTISE PREPARING DATA FOR R. DO IT ON SCHOOL LOG IN IF I NEED EXCEL.
+## STATISTICAL ANALYSIS OF FRONTAL ALPHA ASYMMETRY (FAA) DATA
 
-## LAPLACIAN TRANSFORMED DATA
-
-# LOAD REQUIRE PACKAGES
+# LOAD REQUIRED PACKAGES
 library(readxl)
 library(dplyr)
-# if(!require(devtools)) install.packages("devtools")
-# devtools::install_github("kassambara/ggpubr")
-# install.packages("ggpubr")
-# library(ggpubr)
+library(writexl)
+library(ggpubr)
 
-# IMPORT EO FRONTAL ASYMMETRY SCORES
-exceldir <- "D:/FAA_Study_2021/Skovde/Skovde_EEG/EEG_Statistics/FAAscores_CSD.xls"
-EO_FAA <- read_excel(exceldir, 4, col_names = FALSE);
-EO_FAA <- as.matrix(EO_FAA)
+# PATH
+exportdir <- "D://MPI_LEMON//EEG_MPILMBB_LEMON//EEG_Statistics//FAAscores_CSD.xls"
 
-# SET COLUMN NAMES FOR EASE OF VIEWING
-colnames(EO_FAA) <- c("sub-002","sub-005", "sub-006", "sub-008", "sub-009", "sub-011", "sub-013", 
-                              "sub-014", "sub-015", "sub-019", "sub-020", "sub-021", "sub-022", "sub-025",
-                              "sub-027", "sub-028", "sub-029", "sub-030", "sub-031", "sub-032")
-View(EO_FAA)
 
-# EXTRACT INDIVIDUAL ASYMMETRY SCORES
-EO_FP <- EO_FAA[1,] # eyes-open frontopolar (AF4 - AF3)
-EO_FC <- EO_FAA[2,]   # eyes-open frontocentral (F4 - F3)
-EO_F <- EO_FAA[3,]   # eyes-open frontal (F6 - F5)
-EO_FT <- EO_FAA[4,]   # eyes-open frontotemporal (F8 - F7)
+# IMPORT DATA
+Data <- data.frame(read_excel(exportdir, 1, col_names = TRUE))
+Data$Gender <- factor(Data$Gender) # FOR GENDER, 0 = FEMALE, 1 = MALE
+attach(Data)
+str(Data)
 
 #---------------------------
-## IMPORT EC FRONTAL ASYMMETRY SCORES
-exceldir2 <- "D:/FAA_Study_2021/Skovde/Skovde_EEG/EEG_Statistics/FAAscores_CSD.xls"
-EC_FAA <- read_excel(exceldir, 5, col_names = FALSE);
-EC_FAA <- as.matrix(EC_FAA)
 
-# SET COLUMN NAMES FOR EASE OF VIEWING
-colnames(EC_FAA) <- c("sub-002","sub-005", "sub-006", "sub-008", "sub-009", "sub-011", "sub-013", 
-                      "sub-014", "sub-015", "sub-019", "sub-020", "sub-021", "sub-022", "sub-025",
-                      "sub-027", "sub-028", "sub-029", "sub-030", "sub-031", "sub-032")
-View(EC_FAA)
+# DESCRIPTIVE STATISTICS, INCLUDING CORRELATIONS. PLOT CORR PLOTS (SUBPLOTS)
+traits <- Data[,5:10]
+cortraits <- cor(traits) # CORRELATION MATRIX OF THE BEHAVIOUR SCALES
+cortraits
 
-# EXTRACT INDIVIDUAL ASYMMETRY SCORES
-EC_FP <- EC_FAA[1,] # eyes-open frontopolar (AF4 - AF3)
-EC_FC <- EC_FAA[2,] # eyes-open frontocentral (F4 - F3)
-EC_F <- EC_FAA[3,]  # eyes-open frontal (F6 - F5)
-EC_FT <- EC_FAA[4,]  # eyes-open frontotemporal (F8 - F7)
+# REGRESSION - MIXED-EFFECTS MODELS. ALSO KNOWN AS MULTILEVEL MODELS, 
+# HIERARCHICAL MODELS, AND RANDOM COEFFICIENTS MODELS
+
+# MODEL 1 - FP. WHAT ABOUT TOTAL BAS?
+mod1 <- lm(AF4AF3 ~ BAS.DR, data = Data)
+mod2 <- lm(AF4AF3 ~ BAS.FS, data = Data)
+summary(mod1)
+summary(mod2)
+
+BAS.DR + BAS.FS + BAS.RR + BIS.Anxiety + FFFS.Fear + Gender
+
+# probability of frontal EEG asymmetry given the behavioral variable differs
+# P(EEG asymmetry/behavioral measure)
 
 
-# NORMALITY TESTS USING SHAPIRO-WILKS
-shapiro.test(EO_FP)
-shapiro.test(EO_FC)
-shapiro.test(EO_F)
-shapiro.test(EO_FT)
-shapiro.test(EC_FP)
-shapiro.test(EC_FC)
-shapiro.test(EC_F)
-shapiro.test(EC_FT)
+# CALCULATING SAMPLE SIZE FOR REGRESSION: https://designingexperiments.shinyapps.io/BUCSS_ss_power_reg_all/
 
-# PAIRED-SAMPLES T-TEST TO COMPARE EO AND EC
-t.test(EO_FP, EC_FP, paired = TRUE, alternative = "two.sided")
-t.test(EO_FC, EC_FC, paired = TRUE, alternative = "two.sided")
-t.test(EO_F, EC_F, paired = TRUE, alternative = "two.sided")
-t.test(EO_FT, EC_FT, paired = TRUE, alternative = "two.sided")
+# diagnostic plots
+layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page
+plot(fit)
 
-EO_tot <- EO_FP + EO_FC + EO_F + EO_FT
-EC_tot <- EC_FP + EC_FC + EC_F + EC_FT
-EO_tot <- t(EO_tot)
-EC_tot <- t(EC_tot)
-t.test(EO_tot, EC_tot, paired = TRUE, alternative = "two.sided")
+# RESIDUAL ANALYSIS. CORRECT FOR MULTIPLE TESTS. CHAP 9 COVARIATES
 
-# FAA as predictor instead of outcome? 
+## END OF SCRIPT
